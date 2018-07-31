06ab2098fbd3c2764605094057c856f815a62f37
André Souza Nunes <Andr Souza Nunes>
2018-07-31 00:21:49 -0400

    Ajuste da Classe App.js, inclusão da função validateBook.

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ class BooksApp extends React.Component {
     super(props);
     this.state = {
       books: [],
-      bookAll: [],
       viewHeads:  true,
-      searchView: true
+      booksNew: []
+
     };
     this.updateBooks = this.updateBooks.bind(this)
-    this.boolTrueSearchView = this.boolTrueSearchView.bind(this)
-    this.boolFalseSearchView = this.boolFalseSearchView.bind(this)
+    this.updateBookObject = this.updateBookObject.bind(this)
+    this.onUpdate = this.onUpdate.bind(this)
+
+    // this.boolTrueSearchView = this.boolTrueSearchView.bind(this)
+    // this.boolFalseSearchView = this.boolFalseSearchView.bind(this)
     
     this.self = this
   }
@@ -25,59 +28,107 @@ class BooksApp extends React.Component {
     this.setState({searchView: true})    
     BooksAPI.getAll()
     .then((books) => {
-      this.setState({ books },()=>{
-        this.setState({ bookAll: books})
-      })      
+        this.setState({ books })
     })
   } 
 
-  boolTrueSearchView() {  
-    this.setState({searchView: true })
+  componentDidUpdate(prevProps) {
+    console.log('voltei App',this.state.books)
+    if (this.props.books !== prevProps.books) {
+        this.updateState()
+        console.log('voltei App dentro')
+    }
   }
 
-  boolFalseSearchView() {    
-    this.setState({searchView: false })
+  updateState() {
+    this.setState({books: this.state.books});
+  }
+
+  onUpdate(val , codigo, livros, indice) {
+    console.log('val de App:', val , codigo, livros, indice)
+    console.log('true ou false:',this.validateBook(codigo))
+    if(this.validateBook(codigo)) {
+      this.updateBookObject(val, codigo)
+    } else {
+      this.insertBook(livros[indice])
+    }
+    
+    // this.setState({booksNew: val}, ()=>{
+    //     console.log('BookNews:', this.state.booksNew )
+    //     this.self.setState({books: this.self.state.booksNew})
+    // });
+  }
+
+  validateBook( codigo) {
+    console.log('validateBook:', codigo)
+    let codigoNew = codigo
+    let retorno = false
+    this.state.books.forEach((book)=> {
+      console.log('updateBook KKK:', codigoNew, book.id )
+      if (book.id == codigoNew ){
+        console.log('igual:')
+        retorno = true           
+      }
+    })
+    return retorno
+  }
+
+  insertBook(book) {
+    console.log('Insert book:',book)
+    this.state.books.push(book)
   }
 
   updateBooks(bookNew , updateShelf) {
-    this.state.bookAll.map((book,index)=>{
-      if (book.id === bookNew.id ){
-          let newState = Object.assign({}, this.state);
-          newState.bookAll[index].shelf = updateShelf;
-          BooksAPI.update(book , updateShelf )
-          .then((book) => {
-             return this.self.setState({books: this.state.bookAll })             
-          }) 
+    console.log('voltei:',bookNew, updateShelf)
+    this.state.books.forEach((book)=>{
+        if (book.id === bookNew.id ){
+           let bookId = book.id;  
+           BooksAPI.update(book , updateShelf )
+           .then((book) => {                  
+                this.updateBookObject(updateShelf , bookId )
+           }) 
         }
     })
   }
+
+  updateBookObject(shelfNew , id) {
+    console.log('updateBookObject:',shelfNew, id)
+    this.state.books.forEach((book , index) => {
+        if(book.id == id){
+            let bookUp = book;
+            bookUp.shelf = shelfNew;
+            this.setState({book : bookUp })
+            console.log('Livro:' , shelfNew , id , index, bookUp)
+            console.log('Livro Alterado:' , this.state.books)
+            
+        }
+
+    })
+  }
+    
 
   render() {
 
     return (
       <div className="app">
-        <Route exact path="/" 
-            render={ () => <ListBooks  onUpdateBooks={this.updateBooks} books={this.state.books.filter((book) => book.shelf == "currentlyReading")} titulo="Currently Reading" viewHeads={this.state.viewHeads} />}  />
-      
-        <Route exact path="/" 
-            render={ () => <ListBooks  onUpdateBooks={this.updateBooks} books={this.state.books.filter((book) => book.shelf =="wantToRead")} titulo="Want to Read" />}  />
-        
-        <Route exact path="/" 
-            render={ () => <ListBooks  onUpdateBooks={this.updateBooks} books={this.state.books.filter((book) => book.shelf == "read")} titulo="Read" />}  />
 
-        <Route path="/search" 
-            render={ (props) => <SearchBook {...props} onUpdateBooks={this.updateBooks} books={this.state.books} searchTop="true" searchView={this.boolTrueSearchView}  />}  />
-
-         {this.state.searchView ? (
-             <div className="open-search">
+        <Route exact path="/" render={ () => (
+          <div>
+            <ListBooks  onUpdateBooks={this.updateBooks} books={this.state.books.filter((book) => book.shelf == "currentlyReading")} titulo="Lendo Atualmente" viewHeads={this.state.viewHeads} />
+            <ListBooks  onUpdateBooks={this.updateBooks} books={this.state.books.filter((book) => book.shelf =="wantToRead")} titulo="A ler" />
+            <ListBooks  onUpdateBooks={this.updateBooks} books={this.state.books.filter((book) => book.shelf == "read")} titulo="Lido" />
+            <div className="open-search">
               <Link 
                   to="/search"  
-                  onClick={this.boolFalseSearchView} 
-                  onBoolTrueSearchView={this.boolTrueSearchView}           
-                  className="search-books"
-                  >Add a book</Link>       
+                  className="search-books">
+              </Link>       
             </div> 
-          ):(null)}    
+          </div>
+        )} />
+
+        <Route path="/search" 
+          render={ () => <SearchBook onUpdate={this.onUpdate} books={this.state.books} searchTop="true"   />}  />
+
       </div>
     )
   }
