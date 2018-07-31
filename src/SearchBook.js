765c728ee57c04960f66788192b127d43d06d37f
André Souza Nunes <Andr Souza Nunes>
2018-07-31 00:37:39 -0400

    Ajuste das Classes (SearchBooks), eliminando as mensagens de console.log.

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,10 +1,10 @@
 import React , {Component} from 'react'
+import { debounce } from 'throttle-debounce';
 import { Link } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import escapeRegExp from 'escape-string-regexp'
 import sortyBy from 'sort-by'
 import * as BooksAPI from './BooksAPI'
-
 
 class SearchBook extends Component {
 
@@ -12,76 +12,103 @@ class SearchBook extends Component {
         super(props)
         this.state = {
             query: '',
-            books: [],
-            bookAll: []
+            books: []
         }
 
         this.updateBooks = this.updateBooks.bind(this)
-
+        this.searchBook =  this.searchBook.bind(this)
+        this.searchDebounced =  debounce(1000 , this.completeSearch)
+        
+        this.updateBookObject = this.updateBookObject.bind(this)
         this.self = this
     }
     
-    componentDidMount() {  
-           
-        if(this.state.books.length === 0) {
-            this.setState({ books: this.props.books })
-            this.setState({ bookAll: this.props.books })            
-        }
-    }   
-
-    componentDidUpdate(prevProps) {
-        if (this.props.books !== prevProps.books) {
-           this.setState({books:this.props.books});
-        }
+    componentDidMount() { 
+      BooksAPI.getAll()
+        .then((books) => {
+            this.self.setState({books})    
+        })      
     }
 
-    updateQuery = (query) => {
-        this.setState({ query: query.trim() })
+    completeSearch = q => {
+        this.searchBook(q);
+    };
+
+    onUpdate(valor , codigo, book, indice) {
+        this.props.onUpdate(valor , codigo, book, indice);
     }
+
+    searchBook(e) {
+        let valor = e;
+        this.self.setState({query: valor}, ()=>{
+            BooksAPI.search(valor)
+            .then((search) =>{
+                try {
+                    if (valor)  {
+                        const match = new RegExp(escapeRegExp(valor) , 'i')
+                        this.self.setState({books: search.filter((book) => match.test(valor)) })
+                    }                    
+                }
+                catch (e) { this.self.setState({books: []})  }
+            })
+        })           
+    }
+
+    updateBookObject(updateShelf , id) {
+        let indice = -1;
+        const updatedBooks = this.state.books.map( (book, index) => {
+            if (book.id === id) { 
+                indice = index
+              return {
+                ...book,
+                shelf: updateShelf
+              }
+            }
+            return book
+          })
+
+          this.self.setState({books : updatedBooks }, (idNew)=>{
+            this.onUpdate(updateShelf , id , updatedBooks, indice )
+          })
+    }
+
+    changeQuery = event => {
+        this.setState({ query: event.target.value }, () => {
+          this.searchDebounced(this.state.query);
+        });
+      };
+    
 
     updateBooks(bookNew , updateShelf) {
-    
-        this.state.bookAll.map((book,index)=>{
+        this.state.books.forEach((book)=>{
             if (book.id === bookNew.id ){
-               let newState = Object.assign({}, this.state)     
-               newState.bookAll[index].shelf = updateShelf  
-    
+               let bookId = book.id;  
                BooksAPI.update(book , updateShelf )
                .then((book) => {                  
-                    this.self.setState({books: this.self.bookAll })
+                    this.updateBookObject(updateShelf , bookId )
                }) 
             }
         })
     }
 
     render() {
-            const { onBoolTrueSearchView } = this.props
-            let showBooks
-            if (this.state.query) {
-                const match = new RegExp(escapeRegExp(this.state.query) , 'i')
-                showBooks = this.props.books.filter((book) => match.test(book.title))
-            } else {
-                showBooks = this.props.books
-            }
-
-            showBooks.sort(sortyBy('title'))
-
+            const { onUpdateBooks, onUpdate } = this.props   
             return (
             <div>
                 <div className="search-books">
                     <div className="search-books-bar">
-                        <Link className="close-search" onClick={ (e)=> onBoolTrueSearchView() } to="/">Close</Link>
+                        <Link className="close-search"  to="/">Close</Link>
                         <div className="search-books-input-wrapper">               
                             <input 
                                 type="text" 
                                 placeholder="Search by title or author"
                                 value={this.state.query}
-                                onChange={(event) => this.updateQuery(event.target.value)}/>
+                                onChange={ this.changeQuery }/>
                         </div>
                     </div>
                 </div>             
                 <div className="search-books-results">
-                    <ListBooks  onUpdateBooks={this.updateBooks}  books={ showBooks } searchTop="false" />                  
+                    <ListBooks  onUpdateBooks={this.updateBooks}  books={ this.state.books } searchTop="false" />                  
                 </div>
             </div>
             )
